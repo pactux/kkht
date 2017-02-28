ff246e127da34ae65e5680e0081bdaeb32b36b74
pactux <git@criascript.com.br>
2017-02-28 16:37:51 -0300

    filtragem de acessos por user agent

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -1,11 +1,21 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
-* Classe responsável por criar e encerrar sessões dentro do sistema
+* Classe responsável por criar e encerrar sessões dentro do sistema, 
+* além de filtrar os agentes que acessam
 */
 
 class Login extends CI_Controller {
+	function __construct() {
+		parent::__construct();
+		$this->load->library('user_agent');
+
+		$this->browser = $this->agent->browser();
+		$this->browsers = array('Edge', 'Spartan', 'MSIE', 'Internet Explorer');
+	}
+
 	function index() {
+		if (array_search($this->browser, $this->browsers)) redirect('login/badbrowser');
 		$this->load->view("login_v");
 	}
 
@@ -35,6 +45,11 @@ class Login extends CI_Controller {
 		$this->session->unset_userdata('logado');
 		session_destroy();
 		redirect('login?logout=true', 'location', 301);
+	}
+
+	function badBrowser() {
+		if (array_search($this->browser, $this->browsers) === FALSE) redirect('login');
+		$this->load->view('browser_v');
 	}
 }
 
---
+++ app/views/browser_v.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="robots" content="NOINDEX, NOFOLLOW">
+	<meta name="author" content="pactux">
+
+	<title>Instale um novo browser</title>
+
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/login.css'); ?>" />
+</head>
+
+<body>
+	<div class="container">
+		<h3>Atenção</h3>
+
+		<p>Instale um dos seguintes browsers para uma melhor experiência na web</p>
+
+		<ul class="list-inline">
+			<li>
+				<a href="#" title="Mozilla Firefox">
+					<img src="Firefox" alt="Mozilla Firefox" />
+				</a>
+			</li>
+			<li>
+				<a href="#" title="Google Chrome">
+					<img src="Chrome" alt="Google Chrome" />
+				</a>
+			</li>
+			<li>
+				<a href="#" title="Opera Browser">
+					<img src="Opera" alt="Opera Browser" />
+				</a>
+			</li>
+		</ul>
+	</div>
+</body>
+</html>
