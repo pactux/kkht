922e5048d546b87a1913e650738b314c4ae40c9d
pactux <git@criascript.com.br>
2017-03-02 22:01:54 -0300

    finalização do layout das páginas de erro

--- app/views/errors/html/error_404.php
+++ app/views/errors/html/error_404.php
@@ -1,64 +1,52 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
 <head>
-<meta charset="utf-8">
-<title>404 Page Not Found</title>
-<style type="text/css">
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="robots" content="NOINDEX, NOFOLLOW" />
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
+	<title>404 Página não encontrada</title>
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
+	<style type="text/css">
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
+		::selection { background-color: #E13300; color: white; }
+		::-moz-selection { background-color: #E13300; color: white; }
 
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
+		body {
+			margin: 40px;
+			font: 13px/20px normal Helvetica, Arial, sans-serif;
+			color: #4F5155;
+			background-color: #eeeeee;
+		}
 
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
+		a {
+			font-weight: normal;
+			color: #003399;
+			background-color: transparent;
+		}
 
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
+		h1 {
+			margin-top: 1.8em;
+			margin-bottom: 0.29em;
+			font-size: 9em;
+			text-align: center;
+			color: #444444;
+			background-color: transparent;
+		}
 
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
+		p {
+			margin: 40px -16px 12px 15px;
+			font-size: 15px;
+			text-align: center;
+		}
+
+	</style>
 </head>
 <body>
 	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
+		<h1><b>404</b></h1>
+		<p>Página não encontrada</p>
 	</div>
 </body>
 </html>
